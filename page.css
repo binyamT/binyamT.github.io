a3c8c04af9657ed30f12fd165c8ddc34982c4aef
binyamT <binyamteklebom@gmail.com>
2014-03-20 17:22:56 -0700

    Create page.css

---
+++ page.css
@@ -0,0 +1,17 @@
+
+.zombie {
+	width: 5opx;
+	height: 100px; 
+    color: 'red';
+   text-align:center;
+   background:#33cc66;
+   color:green;
+   font-size: 30pt;
+}
+
+.awesome {
+  background-color:blue;
+  font-size:15pt;
+Head {
+  color:red;
+}
